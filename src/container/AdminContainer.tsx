60f0f0053d4a24045d3ecc73b4c51cf7ab9f53d8
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-21 23:37:50 +0100

    added and changed theme to dark

--- src/container/AdminContainer.tsx
+++ src/container/AdminContainer.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components";
+import { MantineProvider } from "@mantine/core";
 
 
 const AdminContainer: React.FC = () => {
     return (
         <>
-            <div className="admin_container">
-                <Sidebar />
-                <div style={{width:"100%"}}>
-                    <Outlet />
+            <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+                <div data-theme="dark" className="admin_container">
+                    <Sidebar />
+                    <div style={{ width: "100%" }}>
+                        <Outlet />
+                    </div>
                 </div>
-            </div>
+            </MantineProvider>
         </>
     );
 };
