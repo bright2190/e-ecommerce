8be73e1b2027616e76e87bee8fafe27ad38ba755
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-21 22:54:50 +0100

    added pages components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import AdminContainer from "./container/AdminContainer";
+import { AddProduct, Category, Home, Orders, Products } from "./pages";
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
             <Route path="/admin" element={<AdminContainer />}>
-              
+              <Route index element={<Home />}></Route>
+              <Route path="/admin/products" element={<Products />}></Route>
+              <Route path="/admin/products/category" element={<Category />}></Route>
+              <Route path="/admin/products/add-product" element={<AddProduct />}></Route>
+              <Route path="/admin/orders" element={<Orders />}></Route>
             </Route>
         </Routes>
       </div>
