7fd407c47a123449681a9a264fc3d1db87a1eb5d
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-21 23:51:39 +0100

    created admin notification page

---
+++ src/pages/admin/Notification.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PageHeader } from '../../components';
+
+const Notification: React.FC = () => {
+    return (
+        <>
+            <PageHeader emoji={"🔔"} pageName={"Notification"} />
+            
+        </>
+    );
+};
+
+export default Notification;
