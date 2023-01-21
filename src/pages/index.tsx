c2216418b0920287f8c7990821b3e87444016384
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-21 22:54:24 +0100

    added import from pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import AddProduct from "./admin/AddProduct";
+import Category from "./admin/Category";
+import Home from "./admin/Home";
+import Orders from "./admin/Orders";
+import Products from "./admin/Products";
+
+export { Home, Products, Orders, AddProduct, Category }
