4d532f2e785632081f346079e66635991a6c7fcc
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-21 22:54:03 +0100

    added import from components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
+import OverviewCard from "./layout/OverviewCard";
+import PageHeader from "./layout/PageHeader";
 import Sidebar from "./layout/Sidebar";
 import SidebarNavLink from "./layout/SidebarNavLink";
 
-export { Sidebar, SidebarNavLink }
+export { Sidebar, SidebarNavLink, PageHeader, OverviewCard }
