1ec8d614cba84244314ee89da8e8b5cc067139f6
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-22 21:50:13 +0100

    added more components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
+import Navbar  from "./layout/Navbar";
+import HeroCarouselSlide from "./layout/HeroCarouselSlide";
 import OverviewCard from "./layout/OverviewCard";
 import PageHeader from "./layout/PageHeader";
 import Sidebar from "./layout/Sidebar";
 import SidebarNavLink from "./layout/SidebarNavLink";
 
-export { Sidebar, SidebarNavLink, PageHeader, OverviewCard }
+export { Sidebar, SidebarNavLink, PageHeader, OverviewCard, HeroCarouselSlide, Navbar }
