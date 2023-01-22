e6f9cf2e5e04dd1a325ebb8a4b1b321f127ba059
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-22 21:52:35 +0100

    added home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Home from "./admin/Home";
 import Orders from "./admin/Orders";
 import Products from "./admin/Products";
 import Notification from "./admin/Notification";
+import HomePage from "./clients/HomePage";
 
 
-export { Home, Products, Orders, AddProduct, Category,Notification }
+export { Home, Products, Orders, AddProduct, Category, Notification, HomePage }
