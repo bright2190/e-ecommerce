7bb344cd608cc8e7d6f6f3b27e5bdc439a0692eb
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-23 23:52:02 +0100

    added discountcard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
-import Navbar  from "./layout/Navbar";
+import Navbar from "./layout/Navbar";
 import HeroCarouselSlide from "./layout/HeroCarouselSlide";
 import OverviewCard from "./layout/OverviewCard";
 import PageHeader from "./layout/PageHeader";
 import Sidebar from "./layout/Sidebar";
 import SidebarNavLink from "./layout/SidebarNavLink";
+import DiscountCard from "./layout/DiscountCard";
 
-export { Sidebar, SidebarNavLink, PageHeader, OverviewCard, HeroCarouselSlide, Navbar }
+export { Sidebar, SidebarNavLink, PageHeader, OverviewCard, HeroCarouselSlide, Navbar, DiscountCard }
