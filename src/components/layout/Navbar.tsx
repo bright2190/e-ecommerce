f3fca34e3eac665c68e1963426fc264bf1da113c
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-23 23:50:57 +0100

    added login and sign up button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Flex, TextInput } from '@mantine/core';
+import { Flex, TextInput, Button } from '@mantine/core';
 import { IconHeart, IconSearch, IconShoppingBag } from '@tabler/icons';
 import React from 'react';
 
@@ -6,13 +6,15 @@ import React from 'react';
 const Navbar: React.FC = () => {
     return (
         <>
-             <nav className='homepage_nav'>
+            <nav className='homepage_nav'>
                 <a href="#"><h1>👗Matty</h1></a>
 
                 <Flex align={"center"} gap={20}>
-                <TextInput variant={"default"} radius={50} size={"md"} icon={<IconSearch size={22} />} placeholder={"Search"} />
-                <a href="#"><IconHeart size={25} color={"gray"} /></a>
-                <a href="#"><IconShoppingBag size={25} color={"gray"} /></a>
+                    <Button color={"teal"}>Sign up</Button>
+                    <Button color={"teal"} variant={"outline"}>Sign in</Button>
+                    <TextInput variant={"filled"} radius={5} size={"md"} icon={<IconSearch size={22} />} placeholder={"Search"} />
+                    <a href="#"><IconHeart size={25} color={"gray"} /></a>
+                    <a href="#"><IconShoppingBag size={25} color={"gray"} /></a>
                 </Flex>
             </nav>
         </>
