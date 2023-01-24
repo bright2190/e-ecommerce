775bc3597ba4ffba20ec261a2ced526e05e51c95
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-24 21:51:23 +0100

    created store container

---
+++ src/container/StoreContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar } from '../components';
+import { Outlet } from 'react-router-dom';
+
+interface StoreContainerProps {
+
+}
+
+const StoreContainer: React.FC<StoreContainerProps> = ({ }) => {
+    return (
+        <>
+            <div>
+                <Navbar />
+                <div>
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default StoreContainer;
