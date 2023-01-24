1a850654fb28374ddae1b161a3ddb1968d922094
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-24 23:34:27 +0100

    refactored admin auth container

--- src/container/AuthContainer.tsx
+++ src/container/AuthContainer.tsx
@@ -6,10 +6,12 @@ import { Outlet } from 'react-router-dom';
 const AuthContainer: React.FC = () => {
     return (
         <>
-            <div className={"auth_container"}>
-                <a href=""><h1>Matty</h1></a>
-                <div>
-                    <Outlet />
+            <div className="auth_container">
+                <div className={"auth_content"}>
+                    <a href=""><h1>BennyBags & Wears</h1></a>
+                    <div>
+                        <Outlet />
+                    </div>
                 </div>
             </div>
         </>
