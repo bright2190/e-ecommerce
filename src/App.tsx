e54bf2dd5518bcea58fe7bf9feb1cd4274256b07
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-24 23:35:26 +0100

    added routes for auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import AdminContainer from "./container/AdminContainer";
-import { AddProduct, Category, Home, HomePage, Notification, Orders, ProductPage, Products } from "./pages";
+import { AddProduct, AdminLogin, Category, Home, HomePage, Login, Notification, Orders, ProductPage, Products, SignUp } from "./pages";
 import StoreContainer from "./container/StoreContainer";
+import AuthContainer from "./container/AuthContainer";
 function App() {
   return (
     <Router>
@@ -10,6 +11,12 @@ function App() {
           <Route path="/" element={<StoreContainer />}>
             <Route index element={<HomePage />}></Route>
             <Route path={`/products/:product`} element={<ProductPage />}></Route>
+          </Route>
+
+          <Route path="/auth" element={<AuthContainer />}>
+              <Route  path="/auth/sign-up" element={<SignUp />}></Route>
+              <Route  path="/auth/sign-in" element={<Login />}></Route>
+              <Route  path="/auth/admin" element={<AdminLogin />}></Route>
           </Route>
          
             <Route path="/admin" element={<AdminContainer />}>
